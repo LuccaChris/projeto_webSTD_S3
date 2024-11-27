342945e565f7d1dd51d853f1299b07cea5de7bba
Christian <112213507+LuccaChris@users.noreply.github.com>
2024-11-27 12:27:12 -0300

    27/11-1

---
+++ Proj_n2_S3_2/js/app.js
@@ -0,0 +1,17 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+var body = document.querySelector("body");
+
+btnSignin.addEventListener("click", function () {
+    if (!body.classList.contains("sign-in-js")) {
+        body.classList.remove("sign-up-js");
+        body.classList.add("sign-in-js");
+    }
+});
+
+btnSignup.addEventListener("click", function () {
+    if (!body.classList.contains("sign-up-js")) {
+        body.classList.remove("sign-in-js");
+        body.classList.add("sign-up-js");
+    }
+});
